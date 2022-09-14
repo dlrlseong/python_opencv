82768437efbaa96faeee85156cb2619c7ea6d5c5
dlrlseong <leekisung0212@gmail.com>
2022-09-14 11:28:35 +0900

    'Add_6~8'

---
+++ tasks/6_cv2.calcHist.py
@@ -0,0 +1,21 @@
+# importing required libraries of opencv
+import cv2
+
+# importing library for plotting
+from matplotlib import pyplot as plt
+
+# reads an input image
+img = cv2.imread('imput1.jpeg',0)
+
+# find frequency of pixels in range 0-255
+histr = cv2.calcHist([img],[0],None,[256],[0,256])
+
+# show the plotting graph of an image
+img = cv2.resize(img,dsize=[0,0],fx=0.2,fy=0.2,)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+plt.plot(histr)
+plt.show()
+
+
+#####  image to histogram  #####
