88b0bc4a785899fcf570952c6d47a1ef750353be
dlrlseong <leekisung0212@gmail.com>
2022-09-23 14:55:53 +0900

    [ADD] all json files to mask

--- week3/practice.py
+++ week3/practice.py
@@ -10,24 +10,26 @@ ann_dir = os.path.join(root_dir, "jpeg", "ann")
 mask_dir = os.path.join(root_dir, "jpeg", "mask")
 os.makedirs(mask_dir, exist_ok = True)
 ann_files = os.listdir(ann_dir)
-ann_file = ann_files[0]
-ann_file_path = os.path.join(ann_dir,ann_file)
-with open(ann_file_path) as ann_json_file:
-    ann_json = json.load(ann_json_file)
-# pprint(ann_json)
-size = tuple(ann_json["size"].values())
-# print(size)
-black = np.zeros(size)
+# ann_file = ann_files[0]
+for ann_file in ann_files:
+    mask_file_name = ann_file.replace(".json","")
+    ann_file_path = os.path.join(ann_dir,ann_file)
+    with open(ann_file_path) as ann_json_file:
+        ann_json = json.load(ann_json_file)
+    # pprint(ann_json)
+    size = tuple(ann_json["size"].values())
+    # print(size)
+    black = np.zeros(size)
 
-objects = ann_json["objects"]
+    objects = ann_json["objects"]
 
-for pipe in objects:
-    points = pipe["points"]["exterior"]
-    print(points)
-    for i in range(len(points)-1):
-        cv2.line(black,tuple(points[i]), tuple(points[i+1]), 255, 4)
-        
-cv2.imshow("black",black)
-cv2.waitKey(0)        
+    for pipe in objects:
+        points = pipe["points"]["exterior"]
+        # print(points)
+        for i in range(len(points)-1):
+            cv2.line(black,tuple(points[i]), tuple(points[i+1]), 255, 4)
+    cv2.imwrite(os.path.join(mask_dir,mask_file_name.replace(".jpg",".png")), black)
+print(mask_file_name)
+     
             
             
